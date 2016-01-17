0b5d0c7e94807799cc67935f778596ba3b08fadc
John Murphy <chiefsmurph@gmail.com>
2016-01-17 00:08:11 -0800

    levelcreator package.json serve.rjs ;

---
+++ server.js
@@ -0,0 +1,17 @@
+var express = require('express');
+var util = require('util');
+var app = express();
+var server = require('http').Server(app);
+
+var port = process.env.PORT || 5000; // Use the port that Heroku
+server.listen(port);
+
+console.log('listening for http and socket requests on port ' + port);
+
+var bodyParser = require('body-parser')
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }))
+
+// parse application/json
+app.use(bodyParser.json())
+app.use(express.static(__dirname + '/public'));
