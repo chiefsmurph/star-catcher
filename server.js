afdf95acc9f00b1221c2cc2c6967dc9dd77b7198
John Murphy <chiefsmurph@gmail.com>
2016-01-18 16:53:21 -0800

    idea

--- server.js
+++ server.js
@@ -6,6 +6,7 @@ var server = require('http').Server(app);
 var port = process.env.PORT || 5000; // Use the port that Heroku
 server.listen(port);
 
+var io = require('socket.io')(server);
 console.log('listening for http and socket requests on port ' + port);
 
 var bodyParser = require('body-parser')
@@ -15,3 +16,8 @@ app.use(bodyParser.urlencoded({ extended: false }))
 // parse application/json
 app.use(bodyParser.json())
 app.use(express.static(__dirname + '/public'));
+
+
+io.on('connection', function(socket) {
+  console.log('connection');
+});
